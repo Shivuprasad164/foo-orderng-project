677ee1ebe5e82d9ba0743161cf765cce55adaefc
Shivuprasad164 <105096788+Shivuprasad164@users.noreply.github.com>
2022-07-03 23:27:45 +0530

    Update userdata.py

--- userdata.py
+++ userdata.py
@@ -1,1 +1,1 @@
-{"37": ["shiva", 8971458090, "Indore", "dg0312@gmail.com", "mihika"], "66": ["deepanshu gupta", 987654321, "jirapur", "dg@gmail.com", "mishti"]}
+{"37": ["shiva", 8971458090, "Indore", "dg0312@gmail.com", "kumar"], "66": ["sai gireesh", 7338015935, "jirapur", "dg@gmail.com", "mishti"]}
